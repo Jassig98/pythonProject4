68a7f4ddecb699d122fa4491271b7ee0a4e6a412
jasmi_ugm7gn6 <singjasm@oregonstate.edu>
2022-10-07 19:49:26 -0700

    average.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Author: Jasmine Singh
+# GitHub username: Jassig98
+# Date: October 6, 2022
+# Description: average.py
+
+print("Please enter five numbers.")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+print("The average of those numbers is:")
+print((num_1+num_2+num_3+num_4+num_5)/5)
